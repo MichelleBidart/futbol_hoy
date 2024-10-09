53d90e5339a70e6069bff6d1917f0b8960e42777
Michelle <bidart.michelle@gmail.com>
2024-10-08 22:14:41 -0300

    refactor etl_countries and add tests

--- etl/etl_countries.py
+++ etl/etl_countries.py
@@ -5,35 +5,82 @@ import pandas as pd
 import awswrangler as wr
 from dotenv import load_dotenv
 import os
+from typing import List, Dict
 
 load_dotenv('/opt/airflow/.env')
-#load_dotenv('.env')
 
-def extract_countries():
+def extract_countries() -> list:
     """
-    Extrae datos de países
+    Obtiene los datos de los países de la API de fútbol, los guarda en formato Parquet y retorna la lista de países.
 
     Returns:
-        list: Una lista de diccionarios con los datos de los países extraídos de la API.
+        list: Lista de diccionarios con los datos de los países (formato: code, name, flag).
     """
     url, headers = api_url_configurations.get_api_url_headers()
-    response = requests.get(f'{url}/countries', headers=headers) 
-    if response.status_code !=200:
-        raise Exception(f"response error while traying to bring back {url}/countries ")
-    return response.json()['response']
+
+    response = requests.get(f'{url}/countries', headers=headers)
+    
+    if response.status_code != 200:
+        raise Exception(f"Error al obtener los datos de {url}countries")
+
+    countries = response.json()['response']
+    df_countries = pd.DataFrame(countries)
+
+  
+    parquet_directory = './temp/extract/countries'
+    parquet_path = os.path.join(parquet_directory, 'countries.parquet')
 
 
-def transform_countries(countries):
+    os.makedirs(parquet_directory, exist_ok=True)
+
+
+    df_countries.to_parquet(parquet_path, index=False)
+
+    print(f"Datos de países guardados en formato Parquet en: {parquet_path}")
+    print(countries)
+
+    return countries
+
+
+def transform_countries(countries: List[Dict[str, str]]) -> None:
+    """
+    Valida que las columnas sean las esperadas, que 'code' no sea null, que sea varchar de hasta 10 caracteres,
+    elimina duplicados en la columna 'code', transforma los datos en un DataFrame y crea un archivo Parquet.
+    
+    Args:
+        countries (List[Dict[str, str]]): Lista de diccionarios con datos de países.
+    """
+
+    expected_columns = ['code', 'name', 'flag']
 
     countries_df = pd.DataFrame(countries)
-    
-    os.makedirs('./temp/extract/countries', exist_ok=True)
 
-    countries_df.to_csv(os.getenv('TEMP_CSV_COUNTRIES'), index=False)
+    if countries_df['name'].isnull().any():
+        raise ValueError("La columna 'name' contiene valores nulos, lo cual no está permitido.")
+
+    if not countries_df['code'].apply(lambda x: pd.isna(x) or (isinstance(x, str) and len(x) <= 10)).all():
+        raise ValueError("La columna 'code' debe ser null o un string con un máximo de 10 caracteres.")
+    
+    countries_df.drop_duplicates(subset='code', inplace=True)
+
+    parquet_directory = './temp/extract/countries'
+    parquet_path = os.path.join(parquet_directory, 'countries.parquet')
+
+    os.makedirs(parquet_directory, exist_ok=True)
+
+    countries_df.to_parquet(parquet_path, index=False)
+
+    print(f"Datos guardados en formato Parquet en: {parquet_path}")
+
 
 
 
 def load_to_redshift():
+    """
+    Carga los datos de países desde un archivo Parquet a Redshift.
+    Elimina previamente los datos existentes en la tabla 'country'.
+    """
+
     table_name = 'country'
     conn = redshift_utils.get_redshift_connection()
     schema = redshift_utils.get_schema()
@@ -43,7 +90,9 @@ def load_to_redshift():
         conn.commit()
         print(f"Datos eliminados de la tabla {schema}.{table_name}.")
 
-    df = pd.read_csv(os.getenv('TEMP_CSV_COUNTRIES'))
+    parquet_path = './temp/extract/countries/countries.parquet'
+    df = pd.read_parquet(parquet_path)
+
     wr.redshift.to_sql(
         df=df,
         con=conn,
@@ -55,7 +104,7 @@ def load_to_redshift():
         index=False
     )
 
-    print(f"Datos cargados en la tabla {schema}.{table_name}")
+    print(f"Datos cargados en la tabla {schema}.{table_name} desde el archivo Parquet.")
 
 
 
--- tests/test_etl_countries.py
+++ tests/test_etl_countries.py
@@ -1,27 +1,46 @@
-import sys
-import os
-from etl import etl_countries
-
 from unittest.mock import patch, Mock
+from etl.etl_countries import extract_countries
+from etl.etl_countries import transform_countries    
+import pytest
+import pandas as pd
+import os
 
-
+@patch('etl.etl_countries.requests.get')
+@patch('etl.etl_countries.api_url_configurations.get_api_url_headers')
+def test_extract_countries_failare(mock_get_headers, mock_get):
+    mock_get_headers.return_value = ("http://mockurl.com/", {"Authorization": "test"})
+    
+    mock_get.status_code = 400
+ 
+    
+    with pytest.raises(Exception) as exc_info:
+        extract_countries()
+    
+    assert str(exc_info.value) == "Error al obtener los datos de http://mockurl.com/countries"
 
 @patch('etl.etl_countries.requests.get')
 @patch('etl.etl_countries.api_url_configurations.get_api_url_headers')
 def test_extract_countries_success(mock_get_headers, mock_get):
 
-    mock_get_headers.return_value = ("http://mockurl.com", {"Authorization": "Bearer mocktoken"})
+    mock_get_headers.return_value = ("http://mockurl.com/", {"Authorization": "test"})
+    
+    mock_get.return_value.status_code = 200
 
-    mock_response = Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {
-        'response': [{'name': 'Argentina', 'code': 'AR'}, {'name': 'Brazil', 'code': 'BR'}]
+    mock_get.return_value.json.return_value = {
+        'response': [
+            {'code': 'AR', 'name': 'Argentina', 'flag': 'flag_url_Argentina'},
+            {'code': 'BR', 'name': 'Brazil', 'flag': 'flag_url_Brazil'}
+        ]
     }
-    mock_get.return_value = mock_response
 
 
-    countries = etl_countries.extract_countries()
+    countries = extract_countries()
 
 
-    expected_output = [{'name': 'Argentina', 'code': 'AR'}, {'name': 'Brazil', 'code': 'BR'}]
+    expected_output = [
+        {'code': 'AR', 'name': 'Argentina', 'flag': 'flag_url_Argentina'},
+        {'code': 'BR', 'name': 'Brazil', 'flag': 'flag_url_Brazil'}
+    ]
+    
     assert countries == expected_output
+
