6baa1936b5c7ad29dd5736f2e5833c27e9df508e
Michelle <bidart.michelle@gmail.com>
2024-10-28 12:03:30 -0300

    change return listo in xcom to file path

--- silver/run_silver.py
+++ silver/run_silver.py
@@ -1,12 +1,18 @@
 from silver.transform_data import clean_fixture
 from silver.save_to_redshift import save_fixture_to_database
 from utils import redshift_utils
-import os
+from utils.parquet_operations import read_parquet
+
 
 def run_silver(**kwargs):
-    fixtures = kwargs['ti'].xcom_pull(task_ids='bronze_run')
+    file_path = kwargs['ti'].xcom_pull(task_ids='bronze_run')
+    
+    fixtures = read_parquet(file_path)
+    
     conn = redshift_utils.get_redshift_connection()  
+    
     df_match, df_status = clean_fixture(fixtures, conn)
+    
     if df_match is not None and df_status is not None:
         save_fixture_to_database(df_match, df_status, conn)
 
